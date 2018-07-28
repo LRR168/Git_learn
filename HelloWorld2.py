8be5dc970b015ebba507c0d728a8c614ce1f602d
刘人榕 <26826133@163.com>
2018-07-28 15:26:48 +1000

    git class init

---
+++ HelloWorld2.py
@@ -0,0 +1,15 @@
+#!usr/bin/env python
+#-*- coding:utf-8 _*-
+"""
+@author:liurenrong
+@file: HelloWorld2.py
+@time: 2018/07/25
+"""
+
+if __name__ == "__main__":
+    pass
+
+import pandas as pd
+import numpy as np
+
+print("HelloWorld")
